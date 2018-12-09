24cc4094de36f32f1f4f701783f34cc4d2826ac9
Tobias Lønnerød Madsen <m@dsen.tv>
2018-12-09 22:33:43 +0700

    Day 9 part 1 down, needs optimizing for part 2

---
+++ 2018/AOC.Runner/Day9Tests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using AOC.Solver;
+using Xunit;
+using static AOC.Solver.Day9;
+
+namespace AOC.Runner
+{
+    public class Day9Tests
+    {
+        [Fact]
+        public void Part1()
+        {
+            var result = Day9.SolvePart1(_players, _lastMarble);
+            Assert.Equal(404611, result);
+        }
+
+        [Fact]
+        public void Part2()
+        {
+            var result = Day9.SolvePart1(_players, _lastMarble * 100);
+            Assert.Equal(0, result);
+        }
+
+        [Theory]
+        [InlineData(10, 1618, 8317)]
+        [InlineData(13, 7999, 146373)]
+        [InlineData(17, 1104, 2764)]
+        [InlineData(21, 6111, 54718)]
+        [InlineData(30, 5807, 37305)]
+        public void GameShouldPlay(int players, int lastMarble, int expectedScore)
+        {
+            var game = new MarbleGame(players, lastMarble);
+            game.Play();
+            Assert.Equal(expectedScore, game.LeadingPlayer.score);
+        }
+        
+        private const int _players = 431;
+        private const int _lastMarble = 70950;
+    }
+}
---
+++ 2018/AOC.Solver/Day9.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AOC.Solver
+{
+    public static class Day9
+    {
+        public static int SolvePart1(int players, int lastMarble)
+        {
+            var game = new MarbleGame(players, lastMarble);
+            game.Play();
+            var winner = game.LeadingPlayer;
+            return winner.score;
+        }
+
+        public static int SolvePart2(int players, int lastMarble)
+        {
+            throw new NotImplementedException();
+        }
+
+        public class MarbleGame
+        {
+            private readonly int _lastMarbleScore;
+            private readonly List<int> _marbles;
+            private readonly int[] _players;
+            private int _currentMarbleScore = 0;
+            private int _currentMarblePosition = 0;
+            private int _currentPlayer = 0;
+
+            public MarbleGame(int numberOfPlayers, int lastMarbleScore)
+            {
+                _lastMarbleScore = lastMarbleScore;
+                _marbles = new List<int>(lastMarbleScore + 1){ 0 };
+                _players = new int[numberOfPlayers];
+            }
+
+            public (int player, int score) LeadingPlayer => (Array.IndexOf(_players, _players.Max()), _players.Max());
+
+            public void Play()
+            {
+                while (PlayOne()) {}
+            }
+
+            private bool PlayOne()
+            {
+                _currentMarbleScore += 1;
+                if (_currentMarbleScore % 1000 == 0)
+                {
+                    Debug.WriteLine($"Reached marble score {_currentMarbleScore}!");
+                }
+
+                if (_currentMarbleScore % 23 == 0)
+                {
+                    _players[_currentPlayer] += _currentMarbleScore;
+                    _currentMarblePosition -= 7;
+                    if (_currentMarblePosition < 0)
+                    {
+                        _currentMarblePosition += _marbles.Count;
+                    }
+                    _players[_currentPlayer] += _marbles[_currentMarblePosition];
+                    _marbles.RemoveAt(_currentMarblePosition);
+                }
+                else
+                {
+                    int nextPosition;
+                    if (_marbles.Count == 1)
+                    {
+                        nextPosition = 1;
+                    }
+                    else
+                    {
+                        nextPosition = (_currentMarblePosition + 2) % _marbles.Count;
+                    }
+
+                    if (nextPosition == 0)
+                    {
+                        _marbles.Add(_currentMarbleScore);
+                        _currentMarblePosition = _marbles.Count - 1;
+                    }
+                    else
+                    {
+                        _marbles.Insert(nextPosition, _currentMarbleScore);
+                        _currentMarblePosition = nextPosition;
+                    }
+                }
+                
+                _currentPlayer = (_currentPlayer + 1) % _players.Length;
+
+                if (_currentMarbleScore == _lastMarbleScore) return false;
+                return true;
+            }
+        }
+    }
+}
