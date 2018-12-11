b5c45a387d32a4933c3a2e3d6117c390e38e0669
Tobias Lønnerød Madsen <m@dsen.tv>
2018-12-11 16:07:22 +0700

    Day 11 down!

---
+++ 2018/AOC.Runner/Day11Tests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading.Tasks;
+using AOC.Solver;
+using Xunit;
+using static AOC.Solver.Day11;
+using static AOC.Solver.Day9;
+
+namespace AOC.Runner
+{
+    public class Day11Tests
+    {
+
+        private const int Input = 4455;
+
+        [Fact]
+        public void Part1()
+        {
+            var grid = new PowerGrid(300, 300, Input);
+            var best = grid.EvaluateBestLocation();
+            Assert.Equal((21, 54), best);
+        }
+
+        [Fact]
+        public async Task Part2()
+        {
+            var grid = new PowerGrid(300, 300, Input);
+            var best = await grid.EvaluateBestDynamicLocation();
+            Assert.Equal((0, 0, 0), best);
+        }
+
+        [Theory]
+        [InlineData(300, 300, 18, 90, 269, 16)]
+        [InlineData(300, 300, 42, 232, 251, 12)]
+        public async Task PowerGridShouldEvaluateBestDynamicLocation(
+            int width, int height, int serialNumber, int expectedX, int expectedY, int expectedDimension)
+        {
+            var grid = new PowerGrid(width, height, serialNumber);
+            var best = await grid.EvaluateBestDynamicLocation();
+            Assert.Equal((expectedX, expectedY, expectedDimension), best);
+        }
+
+        [Theory]
+        [InlineData(3, 5, 8, 4)]
+        [InlineData(122, 79, 57, -5)]
+        [InlineData(217, 196, 39, 0)]
+        [InlineData(101, 153, 71, 4)]
+        public void PowerCellShouldCalculateLevel(int x, int y, int serialNumber, int expectedResult)
+        {
+            var cell = new PowerCell(x, y, serialNumber);
+            Assert.Equal(expectedResult, cell.PowerLevel);
+        }
+    }
+}
---
+++ 2018/AOC.Solver/Day11.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AOC.Solver
+{
+    public static class Day11
+    {
+        public class PowerGrid
+        {
+            private readonly PowerCell[,] _cells;
+            private readonly int _width;
+            private readonly int _height;
+
+            public PowerGrid(int width, int height, int serialNumber)
+            {
+                _width = width;
+                _height = height;
+                _cells = new PowerCell[width, height];
+                for (var y = 1; y <= height; y++)
+                {
+                    for (var x = 1; x <= width; x++)
+                    {
+                        _cells[x - 1, y - 1] = new PowerCell(x, y, serialNumber);
+                    }
+                }
+            }
+
+            public (int x, int y) EvaluateBestLocation()
+            {
+                var bestLocation = (0, 0);
+                var bestPower = -999;
+
+                for (var x = 1; x < _width-1; x++)
+                {
+                    for (var y = 1; y < _height-1; y++)
+                    {
+                        var power = 0;
+
+                        for (var i = 0; i < 3; i++)
+                        {
+                            for (var j = 0; j < 3; j++)
+                            {
+                                power += _cells[x-1+i, y-1+j].PowerLevel;
+                            }
+                        }
+
+                        if (power > bestPower) {
+                            bestPower = power;
+                            bestLocation = (x, y);
+                        }
+                    }
+                }
+
+                return bestLocation;
+            }
+
+            public async Task<(int x, int y, int dimension)> EvaluateBestDynamicLocation()
+            {
+                var tasks = new List<Task<(int power, (int x, int y, int dimension) bestPosition)>>();
+
+                for (var x = 1; x <= _width; x++)
+                {
+                    for (var y = 1; y <= _height; y++)
+                    {
+                        var x1 = x;
+                        var y1 = y;
+                        tasks.Add(Task.Factory.StartNew(() => EvaluateAllDimensions(x1, y1)));
+                    }
+                }
+
+                var results = await Task.WhenAll(tasks);
+                var bestResult = results.Max(a => a.power);
+
+                return results.First(a => a.power == bestResult).bestPosition;
+            }
+
+            private (int power, (int x, int y, int dimension) bestPosition) EvaluateAllDimensions(int x, int y)
+            {
+                var bestPower = _cells[x - 1, y - 1].PowerLevel;
+                var bestDimension = 1;
+                var spaceAvailable = Math.Min(_width - x + 1, _height - y + 1);
+
+                var currentPower = bestPower;
+
+                for (var dimension = 2; dimension <= spaceAvailable; dimension++)
+                {
+                    var bottomCoord = (y - 1) + (dimension - 1);
+                    var rightCoord = (x - 1) + (dimension - 1);
+                    // We just need to add the new cells (bottom and right rows)
+                    for (var i = 0; i < dimension; i++) {
+                        try 
+                        {
+                            if (i == dimension - 1)
+                            {
+                                // Just add the single corner
+                                currentPower += _cells[x - 1 + i, y - 1 + i].PowerLevel;
+                            }
+                            else
+                            {
+                                // Add both: bottom then right
+                                currentPower += _cells[x - 1 + i, bottomCoord].PowerLevel;
+                                currentPower += _cells[rightCoord, y - 1 + i].PowerLevel;
+                            }
+                        }
+                        catch
+                        {
+                            throw;
+                        }
+                    }
+
+                    if (currentPower > bestPower)
+                    {
+                        bestPower = currentPower;
+                        bestDimension = dimension;
+                    }
+                }
+                return (bestPower, (x, y, bestDimension));
+            }
+        }
+
+        public class PowerCell
+        {
+            public int X { get; set; }
+            public int Y { get; set; }
+            public int RackId { get; set; }
+            public int PowerLevel { get; set; }
+            
+            public PowerCell(int x, int y, int serialNumber)
+            {
+                X = x;
+                Y = y;
+                RackId = x + 10;
+                PowerLevel = EvaluatePowerLevel(serialNumber);
+            }
+            
+            private int EvaluatePowerLevel(int serialNumber)
+            {
+                var step2 = RackId * Y;
+                var step3 = step2 + serialNumber;
+                var step4 = step3 * RackId;
+                var step5 = step4 < 100 ? 0 : Math.Abs(step4/100%10);
+                var step6 = step5 - 5;
+                
+                return step6;
+            }
+        }
+    }
+}
