46096de703d3eb980586f6e9a5f0f2a73df6014e
Tobias Lønnerød Madsen <m@dsen.tv>
2018-12-16 18:35:52 +0700

    Day 16 down!

---
+++ 2018/AOC.Runner/Day16Tests.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using AOC.Solver;
+using Xunit;
+using Xunit.Abstractions;
+using static AOC.Solver.Day16;
+
+namespace AOC.Runner
+{
+    public class Day16Tests
+    {
+
+        [Fact]
+        public void Part1()
+        {
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(), "Day16Tests.Input");
+            var inputFile = File.ReadAllText(filePath);
+
+            var parser = PrepareParser(inputFile);
+
+            Assert.Equal(646, parser.diffuseSamples);
+        }
+
+        [Fact]
+        public void Part2()
+        {
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(), "Day16Tests.Input");
+            var inputFile = File.ReadAllText(filePath);
+
+            var parser = PrepareParser(inputFile);
+            var intermediateMapping = new Dictionary<string, List<int>>();
+
+            foreach (var potentialMapping in parser.potentialCodes)
+            {
+                foreach (var opcode in potentialMapping.Value)
+                {
+                    if (!intermediateMapping.ContainsKey(opcode))
+                    {
+                        intermediateMapping[opcode] = new List<int>{ potentialMapping.Key };
+                    }
+                    else
+                    {
+                        intermediateMapping[opcode].Add(potentialMapping.Key);
+                    }
+                }
+            }
+
+            var opMapping = new Dictionary<int, string>();
+
+            while (intermediateMapping.Any())
+            {
+                var singleMappings = intermediateMapping.Where(kv => kv.Value.Count == 1).ToList();
+                foreach (var mapping in singleMappings)
+                {
+                    opMapping[mapping.Value[0]] = mapping.Key;
+                    intermediateMapping.Remove(mapping.Key);
+                    foreach (var other in intermediateMapping.Values)
+                    {
+                        other.Remove(mapping.Value[0]);
+                    }
+                }
+            }
+
+            var program = inputFile.Split("\n\n\n\n")[1].Trim()
+                .Split("\n")
+                .Select(inst => inst.Split(" ").Select(int.Parse).ToArray())
+                .Select(inst => new Instruction
+                {
+                    OpCode = opMapping[inst[0]],
+                    A = inst[1],
+                    B = inst[2],
+                    C = inst[3],
+                })
+                .ToArray();
+
+            var result = program.Aggregate(new long[]{0, 0, 0, 0}, (r, i) => TimeTravellingDevice.Execute(r, i));
+
+            Assert.Equal(681, result[0]);
+        }
+
+        private (int diffuseSamples, Dictionary<int, List<string>> potentialCodes) PrepareParser(string inputFile)
+        {
+            var re = new Regex("Before: \\[(\\d+), (\\d+), (\\d+), (\\d+)\\]\\n(\\d+) (\\d+) (\\d+) (\\d+)\\nAfter:  \\[(\\d+), (\\d+), (\\d+), (\\d+)\\]", RegexOptions.Multiline);
+            var match = re.Match(inputFile);
+            var potentialCodes = new Dictionary<int, List<string>>();
+            var matchNo = -1;
+            var diffuseSamples = 0;
+            do
+            {
+                matchNo += 1;
+                Debug.WriteLine($"Evaluating match #{matchNo}..");
+                var parsedGroups = match.Groups.Skip(1).Select(i => int.Parse(i.Value)).ToArray();
+                var input = new long[]
+                {
+                    parsedGroups[0],
+                    parsedGroups[1],
+                    parsedGroups[2],
+                    parsedGroups[3],
+                };
+                var op = parsedGroups[4];
+                var instruction = new Instruction
+                {
+                    A = parsedGroups[5],
+                    B = parsedGroups[6],
+                    C = parsedGroups[7],
+                };
+                var output = new long[]
+                {
+                    parsedGroups[8],
+                    parsedGroups[9],
+                    parsedGroups[10],
+                    parsedGroups[11],
+                };
+                if (!potentialCodes.ContainsKey(op))
+                {
+                    potentialCodes[op] = new List<string>(TimeTravellingDevice.OpCodes);
+                }
+
+                var matchingOpcodes = new List<string>();
+                foreach (var opcode in TimeTravellingDevice.OpCodes)
+                {
+                    var result = TimeTravellingDevice.Execute(input, new Instruction
+                    {
+                        OpCode = opcode,
+                        A = instruction.A,
+                        B = instruction.B,
+                        C = instruction.C,
+                    });
+                    if (Enumerable.SequenceEqual(result, output))
+                    {
+                        matchingOpcodes.Add(opcode);
+                    }
+                }
+                potentialCodes[op] = potentialCodes[op].Intersect(matchingOpcodes).ToList();
+
+                if (matchingOpcodes.Count >= 3)
+                {
+                    diffuseSamples += 1;
+                }
+
+                Debug.WriteLine($"Match #{matchNo} [{op}] has {potentialCodes[op].Count} possible mappings ({matchingOpcodes.Count} matches found for this sample)");
+            } while ((match = match.NextMatch()).Success);
+
+            return (diffuseSamples, potentialCodes);
+        }
+    }
+}
---
+++ 2018/AOC.Solver/Day16.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AOC.Solver
+{
+    public static class Day16
+    {
+        public static class TimeTravellingDevice
+        {
+            public static string[] OpCodes => new string[]
+            {
+                "addr",
+                "addi",
+                "mulr",
+                "muli",
+                "banr",
+                "bani",
+                "borr",
+                "bori",
+                "setr",
+                "seti",
+                "gtir",
+                "gtri",
+                "gtrr",
+                "eqir",
+                "eqri",
+                "eqrr",
+            };
+
+            public static long[] Execute(long[] input, Instruction instruction)
+            {
+                var registers = input.ToArray();
+                switch (instruction.OpCode)
+                {
+                    case "addr":
+                        registers[instruction.C] = registers[instruction.A] + registers[instruction.B];
+                        break;
+                    case "addi":
+                        registers[instruction.C] = registers[instruction.A] + instruction.B;
+                        break;
+                    case "mulr":
+                        registers[instruction.C] = registers[instruction.A] * registers[instruction.B];
+                        break;
+                    case "muli":
+                        registers[instruction.C] = registers[instruction.A] * instruction.B;
+                        break;
+                    case "banr":
+                        registers[instruction.C] = registers[instruction.A] & registers[instruction.B];
+                        break;
+                    case "bani":
+                        registers[instruction.C] = registers[instruction.A] & instruction.B;
+                        break;
+                    case "borr":
+                        registers[instruction.C] = registers[instruction.A] | registers[instruction.B];
+                        break;
+                    case "bori":
+                        registers[instruction.C] = registers[instruction.A] | instruction.B;
+                        break;
+                    case "setr":
+                        registers[instruction.C] = registers[instruction.A];
+                        break;
+                    case "seti":
+                        registers[instruction.C] = instruction.A;
+                        break;
+                    case "gtir":
+                        registers[instruction.C] = instruction.A > registers[instruction.B] ? 1 : 0;
+                        break;
+                    case "gtri":
+                        registers[instruction.C] = registers[instruction.A] > instruction.B ? 1 : 0;
+                        break;
+                    case "gtrr":
+                        registers[instruction.C] = registers[instruction.A] > registers[instruction.B] ? 1 : 0;
+                        break;
+                    case "eqir":
+                        registers[instruction.C] = instruction.A == registers[instruction.B] ? 1 : 0;
+                        break;
+                    case "eqri":
+                        registers[instruction.C] = registers[instruction.A] == instruction.B ? 1 : 0;
+                        break;
+                    case "eqrr":
+                        registers[instruction.C] = registers[instruction.A] == registers[instruction.B] ? 1 : 0;
+                        break;
+                    default: throw new ArgumentOutOfRangeException($"Instruction with opcode {instruction.OpCode} not supported!");
+                }
+                return registers;
+            }
+        }
+
+        public class Instruction
+        {
+            public string OpCode { get; set; }
+            
+            // Input 1
+            public int A { get; set; }
+            // Input 2
+            public int B { get; set; }
+
+            // Output 1
+            public int C { get; set; }
+        }
+    }
+}
