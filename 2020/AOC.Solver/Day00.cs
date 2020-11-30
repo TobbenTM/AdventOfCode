dd9f643a8d2b9cd6ccf3a5bb75b1e9ab9866dd6f
Tobias Lønnerød Madsen <m@dsen.tv>
2020-11-30 18:43:16 +0100

    Ready for 2020!

---
+++ 2020/AOC.Solver/Day00.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace AOC.Solver
+{
+    public static class Day00
+    {
+        public static int SolvePart1()
+        {
+            throw new NotImplementedException("Part 1 not implemented yet!");
+        }
+
+        public static int SolvePart2()
+        {
+            throw new NotImplementedException("Part 2 not implemented yet!");
+        }
+    }
+}
