63a5ec93265417d9443788177b01e9012a6ed608
Tobias Lønnerød Madsen <m@dsen.tv>
2020-12-25 21:10:23 +0100

    Day 20 Part 2 down!

--- 2020/AOC.Solver/Day20.cs
+++ 2020/AOC.Solver/Day20.cs
@@ -22,27 +22,126 @@ namespace AOC.Solver
             var unMatchedImages = input.Split(new[] { "\n\n" }, StringSplitOptions.RemoveEmptyEntries)
                 .Select(image => new Image(image))
                 .ToList();
+            var size = Math.Sqrt(unMatchedImages.Count);
+            var dataSize = (int)size * unMatchedImages[0].DataWidth;
             foreach (var image in unMatchedImages)
             {
                 image.FindAdjacentImages(unMatchedImages);
             }
             var map = new Dictionary<(int x, int y), Image>();
             var initialCorner = unMatchedImages.First(image => image.AdjacentImages.Count(i => i != null) == 2);
+            while (initialCorner.AdjacentImages[1] == null || initialCorner.AdjacentImages[2] == null) initialCorner.Rotate();
             map.Add((0, 0), initialCorner);
             unMatchedImages.Remove(initialCorner);
 
-            throw new NotImplementedException("Part 2 not implemented yet!");
+            for (var y = 0; y < size; y++)
+            {
+                for (var x = 0; x < size; x++)
+                {
+                    if (map.ContainsKey((x, y))) continue;
+                    if (y == 0)
+                    {
+                        var left = map[(x - 1, 0)];
+                        var next = left.AdjacentImages[1];
+                        while (left != next.AdjacentImages[3]) next.Rotate();
+                        if (next.AdjacentImages[0] != null) next.VFlip();
+                        unMatchedImages.Remove(next);
+                        map.Add((x, y), next);
+                    }
+                    else
+                    {
+                        var top = map[(x, y - 1)];
+                        var next = top.AdjacentImages[2];
+                        while (top != next.AdjacentImages[0]) next.Rotate();
+                        if (x == 1 && map[(0, y)].AdjacentImages[1] != next) map[(0, y)].HFlip();
+                        if (x >= 1 && next.AdjacentImages[3] != map[(x - 1, y)]) next.HFlip();
+                        unMatchedImages.Remove(next);
+                        map.Add((x, y), next);
+                    }
+                }
+            }
+
+            var roughSeaMap = new HashSet<(int x, int y)>();
+            foreach (var kv in map)
+            {
+                var roughSea = kv.Value.CalculateRoughSea(kv.Key.x, kv.Key.y);
+                foreach (var point in roughSea)
+                {
+                    roughSeaMap.Add(point);
+                }
+            }
+
+            var seaMonsterPattern = new[]
+            {
+                "                  # ",
+                "#    ##    ##    ###",
+                " #  #  #  #  #  #   ",
+            };
+            var seaMonster = seaMonsterPattern.SelectMany((line, y) => line.Select((c, x) => c == ' ' ? (x: -1, y: -1) : (x, y)))
+                    .Where(x => x.x >= 0)
+                    .ToArray();
+
+            for (var flip = 0; flip < 3; flip++)
+            {
+                if (flip == 1)
+                {
+                    roughSeaMap = FlipHorisontally(roughSeaMap, dataSize);
+                }
+                else if (flip == 2)
+                {
+                    roughSeaMap = FlipVertically(FlipHorisontally(roughSeaMap, dataSize), dataSize);
+                }
+                for (var rotate = 0; rotate < 4; rotate++)
+                {
+                    var monstersFound = FindPatterns(roughSeaMap, seaMonster, dataSize);
+                    if (monstersFound > 0) return roughSeaMap.Count - monstersFound * seaMonster.Count();
+                    roughSeaMap = Rotate(roughSeaMap, dataSize);
+                }
+            }
+
+            throw new InvalidOperationException("No suitable orientations found!");
+        }
+
+        private static HashSet<(int x, int y)> Rotate(HashSet<(int x, int y)> set, int size)
+        {
+            return new HashSet<(int x, int y)>(set.Select(point => (-point.y + size, point.x)));
+        }
+
+        private static HashSet<(int x, int y)> FlipHorisontally(HashSet<(int x, int y)> set, int size)
+        {
+            return new HashSet<(int x, int y)>(set.Select(point => (size - point.x, point.y)));
+        }
+
+        private static HashSet<(int x, int y)> FlipVertically(HashSet<(int x, int y)> set, int size)
+        {
+            return new HashSet<(int x, int y)>(set.Select(point => (point.x, size - point.y)));
+        }
+
+        private static int FindPatterns(HashSet<(int x, int y)> map, (int x, int y)[] pattern, int size)
+        {
+            var numberFound = 0;
+            for (var y = 0; y < size - pattern.Select(p => p.y).Max(); y++)
+            {
+                for (var x = 0; x < size - pattern.Select(p => p.x).Max(); x++)
+                {
+                    if (pattern.All(point => map.Contains((point.x + x, point.y + y))))
+                    {
+                        numberFound += 1;
+                    }
+                }
+            }
+            return numberFound;
         }
 
         private class Image
         {
             public int Id { get; }
-            public ImageOrientation Orientation { get; set; } = ImageOrientation.None;
-            public Image[]? AdjacentImages => Transform(_adjacentImages, Orientation); 
+            public int DataWidth => _data.Length;
+            public Image[] AdjacentImages => _adjacentImages;
 
             private readonly string[] _edges;
             private readonly string[] _data;
-            private Image[]? _adjacentImages;
+            private Image[] _adjacentImages = default!;
 
             public Image(string input)
             {
@@ -73,41 +172,57 @@ namespace AOC.Solver
                     .ToArray();
             }
 
-            private static Image[] Transform(Image[]? adjacentImages, ImageOrientation orientation)
+            public void Rotate()
             {
-                if (adjacentImages == null) return new Image[0];
-                var copy = adjacentImages.ToArray();
-                if (orientation.HasFlag(ImageOrientation.HFlipped))
-                {
-                    var tmp = copy[1];
-                    copy[1] = copy[3];
-                    copy[3] = tmp;
-                }
-                if (orientation.HasFlag(ImageOrientation.VFlipped))
-                {
-                    var tmp = copy[0];
-                    copy[0] = copy[2];
-                    copy[2] = tmp;
-                }
-                if (orientation.HasFlag(ImageOrientation.Rotated))
-                {
-                    var tmp = copy[0];
-                    copy[0] = copy[1];
-                    copy[1] = copy[2];
-                    copy[2] = copy[3];
-                    copy[3] = tmp;
-                }
-                return copy;
+                ShiftArray(_edges);
+                ShiftArray(_adjacentImages);
+                var rotatedData = Enumerable.Range(0, _data.Length)
+                    .Select(i => new string(_data.Select(line => line[_data.Length - i - 1]).ToArray()))
+                    .ToArray();
+                Array.Copy(rotatedData, _data, _data.Length);
             }
-        }
 
-        [Flags]
-        private enum ImageOrientation
-        {
-            None = 0,
-            HFlipped = 1,
-            VFlipped = 2,
-            Rotated = 4,
+            public void HFlip()
+            {
+                var tempEdge = _edges[1];
+                _edges[1] = _edges[3];
+                _edges[3] = tempEdge;
+                _edges[0] = new string(_edges[0].Reverse().ToArray());
+                _edges[2] = new string(_edges[2].Reverse().ToArray());
+                var tempImage = _adjacentImages[1];
+                _adjacentImages[1] = _adjacentImages[3];
+                _adjacentImages[3] = tempImage;
+                var flippedData = _data.Select(line => new string(line.Reverse().ToArray())).ToArray();
+                Array.Copy(flippedData, _data, _data.Length);
+            }
+
+            public void VFlip()
+            {
+                var tempEdge = _edges[0];
+                _edges[0] = _edges[2];
+                _edges[2] = tempEdge;
+                _edges[1] = new string(_edges[1].Reverse().ToArray());
+                _edges[3] = new string(_edges[3].Reverse().ToArray());
+                var tempImage = _adjacentImages[0];
+                _adjacentImages[0] = _adjacentImages[2];
+                _adjacentImages[2] = tempImage;
+                var flippedData = _data.Reverse().ToArray();
+                Array.Copy(flippedData, _data, _data.Length);
+            }
+
+            public (int x, int y)[] CalculateRoughSea(int xOffset, int yOffset)
+            {
+                return _data.SelectMany((line, y) => line.Select((c, x) => c == '.' ? (x: -1, y: -1) : (x: xOffset * _data.Length + x, y: yOffset * _data.Length + y)))
+                    .Where(x => x.x >= 0)
+                    .ToArray();
+            }
+
+            private void ShiftArray<T>(T[] arr)
+            {
+                var temp = arr[0];
+                Array.Copy(arr, 1, arr, 0, arr.Length - 1);
+                arr[arr.Length - 1] = temp;
+            }
         }
     }
 }
