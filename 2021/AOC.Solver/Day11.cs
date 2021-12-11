6302b571d7fc25dfd11faeb18aec3bfe2a39b0ec
Tobias Lønnerød Madsen <m@dsen.tv>
2021-12-11 07:47:22 +0100

    More dry

--- 2021/AOC.Solver/Day11.cs
+++ 2021/AOC.Solver/Day11.cs
@@ -19,64 +19,18 @@ namespace AOC.Solver
 
         public static int SolvePart1(int[][] input)
         {
-            var octopuses = new Dictionary<(int row, int col), int>();
-
-            for (var row = 0; row < input.Length; row++)
-            {
-                for (var col = 0; col < input[row].Length; col++)
-                {
-                    octopuses.Add((row, col), input[row][col]);
-                }
-            }
-
+            var octopuses = ToMap(input);
             var flashes = 0;
-
             for (var step = 0; step < 100; step++)
             {
-                foreach (var octopus in octopuses)
-                {
-                    octopuses[octopus.Key] = octopus.Value + 1;
-                }
-
-                while (octopuses.Values.Any(energy => energy > 9))
-                {
-                    var flashingOctopus = octopuses.First(kv => kv.Value > 9);
-                    octopuses[flashingOctopus.Key] = -1;
-                    flashes += 1;
-
-                    foreach (var neighbour in Neighbours)
-                    {
-                        var pos = (neighbour.deltaRow + flashingOctopus.Key.row,
-                            neighbour.deltaCol + flashingOctopus.Key.col);
-                        if (octopuses.TryGetValue(pos, out var value) && value != -1)
-                        {
-                            octopuses[pos] += 1;
-                        }
-                    }
-                }
-
-                foreach (var octopus in octopuses.Where(kv => kv.Value == -1))
-                {
-                    octopuses[octopus.Key] = 0;
-                }
+                flashes += RunStep(octopuses);
             }
-
             return flashes;
         }
 
         public static int SolvePart2(int[][] input)
         {
-
-            var octopuses = new Dictionary<(int row, int col), int>();
-
-            for (var row = 0; row < input.Length; row++)
-            {
-                for (var col = 0; col < input[row].Length; col++)
-                {
-                    octopuses.Add((row, col), input[row][col]);
-                }
-            }
-
+            var octopuses = ToMap(input);
             var steps = 0;
             while (true)
             {
@@ -85,34 +39,59 @@ namespace AOC.Solver
                     return steps;
                 }
 
-                foreach (var octopus in octopuses)
-                {
-                    octopuses[octopus.Key] = octopus.Value + 1;
-                }
-
-                while (octopuses.Values.Any(energy => energy > 9))
-                {
-                    var flashingOctopus = octopuses.First(kv => kv.Value > 9);
-                    octopuses[flashingOctopus.Key] = -1;
-
-                    foreach (var neighbour in Neighbours)
-                    {
-                        var pos = (neighbour.deltaRow + flashingOctopus.Key.row,
-                            neighbour.deltaCol + flashingOctopus.Key.col);
-                        if (octopuses.TryGetValue(pos, out var value) && value != -1)
-                        {
-                            octopuses[pos] += 1;
-                        }
-                    }
-                }
-
-                foreach (var octopus in octopuses.Where(kv => kv.Value == -1))
-                {
-                    octopuses[octopus.Key] = 0;
-                }
+                RunStep(octopuses);
 
                 steps += 1;
             }
+        }
+
+        private static Dictionary<(int row, int col), int> ToMap(int[][] input)
+        {
+            var octopuses = new Dictionary<(int row, int col), int>();
+
+            for (var row = 0; row < input.Length; row++)
+            {
+                for (var col = 0; col < input[row].Length; col++)
+                {
+                    octopuses.Add((row, col), input[row][col]);
+                }
+            }
+
+            return octopuses;
+        }
+
+        private static int RunStep(Dictionary<(int row, int col), int> octopuses)
+        {
+            var flashes = 0;
+
+            foreach (var octopus in octopuses)
+            {
+                octopuses[octopus.Key] = octopus.Value + 1;
+            }
+
+            while (octopuses.Values.Any(energy => energy > 9))
+            {
+                var flashingOctopus = octopuses.First(kv => kv.Value > 9);
+                octopuses[flashingOctopus.Key] = -1;
+                flashes += 1;
+
+                foreach (var neighbour in Neighbours)
+                {
+                    var pos = (neighbour.deltaRow + flashingOctopus.Key.row,
+                        neighbour.deltaCol + flashingOctopus.Key.col);
+                    if (octopuses.TryGetValue(pos, out var value) && value != -1)
+                    {
+                        octopuses[pos] += 1;
+                    }
+                }
+            }
+
+            foreach (var octopus in octopuses.Where(kv => kv.Value == -1))
+            {
+                octopuses[octopus.Key] = 0;
+            }
+
+            return flashes;
         }
     }
 }
