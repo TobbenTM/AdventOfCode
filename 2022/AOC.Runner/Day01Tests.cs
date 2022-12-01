a9c21781366fe3eda9f958f7ba19375608ac7883
Tobias Lønnerød Madsen <m@dsen.tv>
2022-12-01 09:09:21 +0100

    Update C# style

--- 2022/AOC.Runner/Day01Tests.cs
+++ 2022/AOC.Runner/Day01Tests.cs
@@ -2,29 +2,28 @@ using System.IO;
 using AOC.Solver;
 using Xunit;
 
-namespace AOC.Runner
+namespace AOC.Runner;
+
+public class Day01Tests
 {
-    public class Day01Tests
+    private readonly string _input;
+
+    public Day01Tests()
     {
-        private readonly string _input;
+        _input = File.ReadAllText("./Day01.input");
+    }
 
-        public Day01Tests()
-        {
-            _input = File.ReadAllText("./Day01.input");
-        }
+    [Fact]
+    public void Part1()
+    {
+        var result = Day01.SolvePart1(_input);
+        Assert.Equal(67658, result);
+    }
 
-        [Fact]
-        public void Part1()
-        {
-            var result = Day01.SolvePart1(_input);
-            Assert.Equal(67658, result);
-        }
-
-        [Fact]
-        public void Part2()
-        {
-            var result = Day01.SolvePart2(_input);
-            Assert.Equal(200158, result);
-        }
+    [Fact]
+    public void Part2()
+    {
+        var result = Day01.SolvePart2(_input);
+        Assert.Equal(200158, result);
     }
 }
--- 2022/AOC.Solver/Day01.cs
+++ 2022/AOC.Solver/Day01.cs
@@ -1,18 +1,17 @@
 using System;
 using System.Linq;
 
-namespace AOC.Solver
-{
-    public static class Day01
-    {
-        public static long SolvePart1(string input)
-        {
-            return input.Split("\n\n").Select(e => e.Split("\n", StringSplitOptions.RemoveEmptyEntries).Select(long.Parse).Sum()).Max();
-        }
+namespace AOC.Solver;
 
-        public static long SolvePart2(string input)
-        {
-            return input.Split("\n\n").Select(e => e.Split("\n", StringSplitOptions.RemoveEmptyEntries).Select(long.Parse).Sum()).OrderByDescending(n => n).Take(3).Sum();
-        }
+public static class Day01
+{
+    public static long SolvePart1(string input)
+    {
+        return input.Split("\n\n").Select(e => e.Split("\n", StringSplitOptions.RemoveEmptyEntries).Select(long.Parse).Sum()).Max();
+    }
+
+    public static long SolvePart2(string input)
+    {
+        return input.Split("\n\n").Select(e => e.Split("\n", StringSplitOptions.RemoveEmptyEntries).Select(long.Parse).Sum()).OrderByDescending(n => n).Take(3).Sum();
     }
 }
